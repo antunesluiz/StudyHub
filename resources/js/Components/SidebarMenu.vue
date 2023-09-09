8faa62ce57662a8b665709188944ae02a9b023e1
Luiz Antunes <luiz.antunes@codificar.com.br>
2023-09-09 18:59:35 -0300

    iniciando front table estudantes

--- resources/js/Components/SidebarMenu.vue
+++ resources/js/Components/SidebarMenu.vue
@@ -46,7 +46,7 @@ onMounted(() => {
                 <li>
                     <button type="button"
                         class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                        aria-controls="dropdown-users" data-collapse-toggle="dropdown-users">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                             <path
@@ -59,10 +59,11 @@ onMounted(() => {
                                 d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
-                    <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                    <ul id="dropdown-users" class="hidden py-2 space-y-2">
                         <li>
                             <a :href="route('admin.student.index')"
-                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Estudantes</a>
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                :class="route().current('admin.student.index') ? 'bg-gray-100 dark:bg-gray-700' : ''">Estudantes</a>
                         </li>
                         <li>
                             <a href="#"
--- resources/js/Pages/Admin/User/Student/Index.vue
+++ resources/js/Pages/Admin/User/Student/Index.vue
@@ -1,9 +1,24 @@
 <script setup>
 import AdminLayout from '@/Layouts/AdminLayout.vue';
-
+import TopNavigation from './Partials/TopNavigation.vue';
+import SearchInput from './Partials/SearchInput.vue'
+import StudentTable from './Partials/StudentTable.vue'
 </script>
 
 <template>
     <AdminLayout title="Dashboard" class="sm:ml-64 bg-gray-100 dark:bg-gray-900 pt-4">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Estudantes
+            </h2>
+        </template>
+
+        <div class="bg-gray-300 dark:bg-gray-700 py-8 px-6 sm:px-6 dark:text-gray-50 mt-4 rounded mx-4">
+            <TopNavigation></TopNavigation>
+            <div class="mt-4 flex flex-wrap flex-row">
+                <SearchInput></SearchInput>
+            </div>
+            <StudentTable></StudentTable>
+        </div>
     </AdminLayout>
 </template>
